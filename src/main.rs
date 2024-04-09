dbe83e0c88fe01c61668702064429d74ba671c96
linrongbin16 <linrongbin16@outlook.com>
2024-04-09 09:52:20 +0800

    feat(main): add hello with crossterm (#65)
    build(Cargo.lock): include Cargo.lock to git (#65)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use cargo_dependabot_test;
+use std::io::{stdout, Write};
 
-fn main() {
-  cargo_dependabot_test::hello();
+use crossterm::{
+    event, execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand,
+};
+
+fn main() -> std::io::Result<()> {
+    // using the macro
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Red),
+        Print("Styled text here."),
+        ResetColor
+    )?;
+
+    cargo_dependabot_test::hello();
+
+    Ok(())
 }
