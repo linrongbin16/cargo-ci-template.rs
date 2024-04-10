135034bd334c74030c38fec092bd52a5ef7cc5be
linrongbin16 <linrongbin16@outlook.com>
2024-04-10 16:37:46 +0800

    ci(build): try manual build again4

--- build-tarball-win.sh
+++ build-tarball-win.sh
@@ -7,9 +7,12 @@ target="${INPUT_TARGET}"
 bin="${INPUT_BIN}"
 checksum=${INPUT_CHECKSUM}
 archive="$bin-$target"
-command="cargo"
+target_lower="${target//-/_}"
+target_lower="${target_lower//./_}"
+target_upper=$(tr '[:lower:]' '[:upper:]' <<<"${target_lower}")
 
 host=$(rustc -Vv | grep 'host: ' | cut -c 7-)
+command="cargo"
 
 if [[ "${host}" != "${target}" ]]; then
 	command="cross"
