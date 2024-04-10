940d4204762e39428c3731b8a2f13f23ea02863f
linrongbin16 <linrongbin16@outlook.com>
2024-04-10 21:37:22 +0800

    feat(bin): add multiple bins

---
+++ src/bin/hello.rs
@@ -0,0 +1,21 @@
+use cargo_dependabot_test;
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+};
+use std::io::stdout;
+
+fn main() -> std::io::Result<()> {
+    // using the macro
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Red),
+        Print("Hello."),
+        ResetColor
+    )?;
+
+    cargo_dependabot_test::hello();
+
+    Ok(())
+}
