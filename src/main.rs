95b2839e44fc9feb7de28cb7f83cad50f8c31aa0
linrongbin16 <linrongbin16@outlook.com>
2024-04-29 12:46:13 +0800

    chore

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use crossterm::{
     execute,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
 };
+use heed::types as heed_types;
+use heed::{byteorder, Database, EnvOpenOptions};
 use std::io::stdout;
+use std::path::Path;
 
 fn main() -> std::io::Result<()> {
     // using the macro
@@ -16,6 +19,13 @@ fn main() -> std::io::Result<()> {
     )?;
 
     cargo_dependabot_test::hello();
+
+    let env = unsafe { EnvOpenOptions::new().open(Path::new(".")).unwrap() };
+    let mut wtxn = env.write_txn().unwrap();
+    let db: Database<heed_types::Str, heed_types::U32<byteorder::NativeEndian>> =
+        env.create_database(&mut wtxn, None).unwrap();
+    db.put(&mut wtxn, "seven", &7).unwrap();
+    wtxn.commit().unwrap();
 
     Ok(())
 }
