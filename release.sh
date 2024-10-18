b8dba48a2134562d6dabad41d27be87df37adc01
linrongbin16 <linrongbin16@outlook.com>
2024-10-19 07:04:25 +0800

    feat: update release

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+if [ $# -lt 1 ]; then
+	echo "usage: ./release.sh [LEVEL] (--execute --no-verify)"
+	echo "error: missing release arguments, exit..."
+	exit 1
+fi
+
+export GIT_CLIFF_CONFIG=$PWD/cliff.toml
+export GIT_CLIFF_WORKDIR=$PWD
+export GIT_CLIFF_REPOSITORY=$PWD
+
+cargo release "$@"
+
+# git-cliff will create "CHANGELOG.md" for each packages, but we only need the one from cargo_ci_template_cli.
+# rm rsvim_core/CHANGELOG.md
+# cp cargo_ci_template_cli/CLI_CHANGELOG.md CHANGELOG.md
+
