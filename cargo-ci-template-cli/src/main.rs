b48dbf7bd2c16b3f0450a0acc426856ef3448205
linrongbin16 <linrongbin16@outlook.com>
2025-03-25 11:54:51 +0800

    perf: remove crossterm deps

--- cargo-ci-template-cli/src/main.rs
+++ cargo-ci-template-cli/src/main.rs
@@ -1,18 +1,3 @@
-use crossterm::{
-    execute,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-};
-use std::io::stdout;
-
 fn main() -> std::io::Result<()> {
-    // using the macro
-    execute!(
-        stdout(),
-        SetForegroundColor(Color::Blue),
-        SetBackgroundColor(Color::Red),
-        Print("Styled text here."),
-        ResetColor
-    )?;
-
     Ok(())
 }
